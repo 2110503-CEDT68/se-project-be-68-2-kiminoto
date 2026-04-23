c25632c3f4afb4e74f85e5396ee0de4840772266
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2026-04-23 20:27:31 +0700

    feat: add delete field controller for profile

--- controllers/profile.js
+++ controllers/profile.js
@@ -25,3 +25,28 @@ exports.editProfileField = async (req, res, next) => {
         res.status(400).json({ success: false, error: err.message });
     }
 };
+
+exports.deleteProfileField = async (req, res, next) => {
+    try {
+        const { key } = req.body;
+
+        const existingKeys = req.user.profile.fields.map((data) => data.key);
+
+        if (existingKeys.includes(key)) {
+            // field does exist, so update it.
+            const index = existingKeys.indexOf(key);
+            req.user.profile.fields.splice(index, 1);
+        } else {
+            // field doesn't exist, so create it.
+            res.status(404).json({ success: false, error: "Field not found." });
+            return;
+        }
+
+        await req.user.save();
+
+        // TODO: Figure out what to return instead.
+        res.status(200).json({ success: true, data: req.user });
+    } catch (err) {
+        res.status(400).json({ success: false, error: err.message });
+    }
+};
