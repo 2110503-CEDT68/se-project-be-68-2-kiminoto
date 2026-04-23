f4197cc04b6844aea48ad7d781d55dccefc9773f
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2026-04-23 20:27:26 +0700

    feat: add delete controller to route

--- routes/profile.js
+++ routes/profile.js
@@ -3,10 +3,14 @@ const router = express.Router();
 
 const { protect, authorize } = require("../middleware/auth");
 
-const { editProfileField } = require("../controllers/profile");
+const {
+    editProfileField,
+    deleteProfileField,
+} = require("../controllers/profile");
 
 router
     .route("/fields")
-    .patch(protect, authorize("admin", "user"), editProfileField);
+    .patch(protect, authorize("admin", "user"), editProfileField)
+    .delete(protect, authorize("admin", "user"), deleteProfileField);
 
 module.exports = router;
