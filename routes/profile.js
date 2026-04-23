c7ec346298e6e9310f52d31466cb2086bece5c1b
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2026-04-23 20:18:25 +0700

    feat: add imports and exports to profile route

--- routes/profile.js
+++ routes/profile.js
@@ -3,6 +3,10 @@ const router = express.Router();
 
 const { protect, authorize } = require("../middleware/auth");
 
+const { editProfileField } = require("../controllers/profile");
+
 router
     .route("/fields")
     .patch(protect, authorize("admin", "user"), editProfileField);
+
+module.exports = router;
